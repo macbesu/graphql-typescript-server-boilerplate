7409e465b37af127043951f8dcd7fc9ab4121d3d
ivanwarke <iveon.cc@gmail.com>
2018-06-19 21:54:09 +0800

    [Add] README.md

--- src/tests/register.test.ts
+++ src/tests/register.test.ts
@@ -1,8 +1,10 @@
 import { request } from "graphql-request";
+import { createConnection } from "typeorm";
 
 import { host } from "./constants";
+import { User } from "../entity/User";
   
-const email = "ivan@huya.com";
+const email = "ivan1@huya.com";
 const password = "matt10801";
 
 const mutation = `
@@ -14,4 +16,10 @@ mutation {
 test("Register", async () => {
   const res = await request(host, mutation);
   expect(res).toEqual({ register: true });
+  await createConnection();
+  const users = await User.find({ where: { email } });
+  expect(users).toHaveLength(1);
+  const user = users[0];
+  expect(user.email).toEqual(email);
+  expect(user.password).not.toEqual(password);
 });
