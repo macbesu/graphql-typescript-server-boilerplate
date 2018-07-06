81dd239d5d5b0fa5365159d0b3e6b83c596592b1
ivanwarke <iveon.cc@gmail.com>
2018-07-06 18:18:29 +0800

    handling errors in graphql

--- src/modules/register/register.test.ts
+++ src/modules/register/register.test.ts
@@ -11,21 +11,28 @@ beforeAll(async () => {
   getHost = () => `http://127.0.0.1:${port}`;
 });
   
-const email = "ivan2@huya.com";
+const email = "ivan3@huya.com";
 const password = "matt10801";
 
 const mutation = `
 mutation {
-  register(email: "${email}", password:"${password}")
+  register(email: "${email}", password:"${password}") {
+    path
+    message
+  }
 }
 `;
 
 test("Register", async () => {
   const res = await request(getHost(), mutation);
-  expect(res).toEqual({ register: true });
+  expect(res).toEqual({ register: null });
   const users = await User.find({ where: { email } });
   expect(users).toHaveLength(1);
   const user = users[0];
   expect(user.email).toEqual(email);
   expect(user.password).not.toEqual(password);
+
+  const res2: any = await request(getHost(), mutation);
+  expect(res2.register).toHaveLength(1);
+  expect(res2.register[0].path).toEqual('email');
 });
--- src/modules/register/resolvers.ts
+++ src/modules/register/resolvers.ts
@@ -4,17 +4,35 @@ import { User } from '../../entity/User';
 
 export const resolvers: ResolverMap = {
   Query: {
-    bye: () => 'hi',
+    bye: () => 'bye',
   },
   Mutation: {
-    register: async (_: any, { email, password }: GQL.IRegisterOnMutationArguments) => {
+    register: async (
+      _: any,
+      { email, password }: GQL.IRegisterOnMutationArguments
+    ) => {
+      const userAlreadyExists = await User.findOne({
+        where: { email },
+        select: ['id'],
+      });
+
+      if (userAlreadyExists) {
+        return [
+          {
+            path: 'email',
+            message: 'already taken'
+          }
+        ];
+      }
+
       const hashedPassword = await bcrypt.hash(password, 10);
       const user = User.create({
         email,
         password: hashedPassword,
       });
+      
       await user.save();
-      return true;
+      return null;
     }
   }
 };
