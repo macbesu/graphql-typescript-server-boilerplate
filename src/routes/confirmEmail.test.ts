3b8ad9798355b428f48aa49156612bb4983afacc
ivanwarke <iveon.cc@gmail.com>
2018-07-07 17:41:38 +0800

    fix connection error

--- src/routes/confirmEmail.test.ts
+++ src/routes/confirmEmail.test.ts
@@ -3,5 +3,5 @@ import fetch from 'node-fetch';
 test('sends invalid back if bad id sent', async () => {
   const res = await fetch(`${process.env.TEST_HOST}/confirm/12083`);
   const text = await res.text();
-  expect(text).toEqual('invalid');
+  expect(text).toEqual('invalid'); 
 });
--- src/runServer.ts
+++ src/runServer.ts
@@ -1,27 +1,13 @@
-import { importSchema } from "graphql-import";
 import { GraphQLServer } from "graphql-yoga";
-import * as path from "path";
-import * as fs from "fs";
-import { mergeSchemas, makeExecutableSchema } from 'graphql-tools';
-import { GraphQLSchema } from "graphql";
 
 import { createTypeormConn } from "./utils/createTypeormConn";
 import { redis } from './redis';
 import { confirmEmail } from './routes/confirmEmail';
+import { genSchema } from "./utils/genSchema";
 
 export const runServer = async () => {
-  const schemas: GraphQLSchema[] = [];
-  const folders = fs.readdirSync(path.join(__dirname, "./modules"));
-  folders.forEach(folder => {
-    const { resolvers } = require(`./modules/${folder}/resolvers`);
-    const typeDefs = importSchema(
-      path.join(__dirname, `./modules/${folder}/schema.graphql`)
-    );
-    schemas.push(makeExecutableSchema({ resolvers, typeDefs }))
-  });
-
   const server = new GraphQLServer({
-    schema: mergeSchemas({ schemas }),
+    schema: genSchema(),
     context: ({ request }) => ({
       redis,
       url: request.protocol + '://' + request.get('host'),
